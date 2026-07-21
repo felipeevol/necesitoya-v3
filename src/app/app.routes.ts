febf751a72d5426dfb123806c8aedfaf5bfcd579
Felipe Camargo Machado <felipe.camargo@gmail.com>
2026-07-22 01:23:50 +0200

    Politica de privacidade

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'politica-de-privacidade',
+    component: PrivacyPolicyComponent,
+  },
+];
