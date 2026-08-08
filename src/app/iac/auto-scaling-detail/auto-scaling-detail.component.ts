744313c880a34be9a17a3466e03bac273c33e149
Felipe Camargo Machado <felipe.camargo@gmail.com>
2026-08-08 17:18:43 +0200

    Adicionei secao de o que fazer no autoscaling

--- src/app/iac/auto-scaling-detail/auto-scaling-detail.component.ts
+++ src/app/iac/auto-scaling-detail/auto-scaling-detail.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Translation } from '../../shared/site-translations';
+import { CommandGroup, CommandPlatform, getIacCommand } from '../iac-command.model';
 
 @Component({
   selector: 'app-auto-scaling-detail',
@@ -11,4 +12,31 @@ import { Translation } from '../../shared/site-translations';
 })
 export class AutoScalingDetailComponent {
   @Input({ required: true }) currentPage!: Translation['iacPage'];
+  @Input({ required: true }) activeCommandPlatform!: Record<CommandGroup, CommandPlatform>;
+  @Input() copiedCommand: CommandGroup | null = null;
+
+  @Output() commandPlatformChange = new EventEmitter<{
+    group: CommandGroup;
+    platform: CommandPlatform;
+  }>();
+  @Output() commandCopy = new EventEmitter<{
+    group: CommandGroup;
+    platform?: CommandPlatform;
+  }>();
+
+  isCommandPlatformActive(group: CommandGroup, platform: CommandPlatform): boolean {
+    return this.activeCommandPlatform[group] === platform;
+  }
+
+  setCommandPlatform(group: CommandGroup, platform: CommandPlatform): void {
+    this.commandPlatformChange.emit({ group, platform });
+  }
+
+  getCommand(group: CommandGroup, platformOverride?: CommandPlatform): string {
+    return getIacCommand(group, platformOverride ?? this.activeCommandPlatform[group]);
+  }
+
+  copyCommand(group: CommandGroup, platform?: CommandPlatform): void {
+    this.commandCopy.emit({ group, platform });
+  }
 }
